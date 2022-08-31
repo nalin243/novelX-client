04556157ff07e3c4faf412bc5f689f3705e54fd3
nalin243 <devpriyanalin@gmail.com>
2022-08-31 20:47:15 +0530

    improved folder structure

---
+++ client/src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer(props){
+	return (
+		<div class="bg-black text-center mt-auto border p-4">
+			<h4 class="text-white">© 2022 {props.name}™ .All rights reserved.</h4>
+		</div>
+		)
+}
+
+export default Footer
