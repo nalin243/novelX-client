3e7af924717392934093eb352e29c3dc278c10e1
nalin243 <devpriyanalin@gmail.com>
2022-08-31 14:23:20 +0530

    added header and footer for index page

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+function App(props){
+	return (
+		<div class="flex flex-wrap flex-col min-h-screen">
+			<Navbar name={props.name}/>
+
+			<Footer name={props.name}/>
+		</div>
+	)
+}
+
+export default App
---
+++ client/src/Footer.js
@@ -0,0 +1,9 @@
+function Footer(props){
+	return (
+		<div class="text-center mt-auto border shadow-md border-gray-200 p-4">
+			<h4 class="text-gray-700">© 2022 {props.name}™ .All rights reserved.</h4>
+		</div>
+		)
+}
+
+export default Footer
