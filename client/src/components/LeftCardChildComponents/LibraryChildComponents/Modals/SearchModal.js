7eba44428ed07beaf57f7c5128bdcbb29d257e28
nalin243 <devpriyanalin@gmail.com>
2022-09-16 17:56:57 +0530

    Added button to add book to library

--- client/src/components/LeftCardChildComponents/LibraryChildComponents/Modals/SearchModal.js
+++ client/src/components/LeftCardChildComponents/LibraryChildComponents/Modals/SearchModal.js
@@ -1,31 +1,60 @@
+
+import axios from "axios"
+
 function SearchModal(props) {
 
 	const searchResult = props.searchResult
 
-		const book = new Map()
+	const bookDetails = new Map()
 
-		try{
-		book.set("title",(searchResult.volumeInfo).title)
-		book.set("author",(searchResult.volumeInfo.authors)[0])
-		book.set("pageCount",searchResult.volumeInfo.pageCount)
-		book.set("description",searchResult.volumeInfo.description)
+	try{
+		bookDetails.set("title",(searchResult.volumeInfo).title)
+		bookDetails.set("author",(searchResult.volumeInfo.authors)[0])
+		bookDetails.set("pageCount",searchResult.volumeInfo.pageCount)
+		bookDetails.set("description",searchResult.volumeInfo.description)
+	}
+	catch(error){
+		console.log(error)
+	}
+
+	const Book = {
+		name: bookDetails.get("title"),
+		Pages: bookDetails.get("pageCount"),
+		Author: bookDetails.get("author"),
+		Description: bookDetails.get("description")
+	}
+
+	function addToLibrary(Book){
+		const query = new URLSearchParams("?"+(window.location.href).split('?')[1])
+		const username = query.get("user")
+
+		const data = {
+			username: username, 
+			book: Book
 		}
-		catch(error){
-			console.log(error)
-		}
+
+		axios.post("http://localhost:3001/updateLibrary",data)
+	}	
+
+
 
 	return (
 		<div onMouseLeave={()=>props.updateAnimation("")} class="flex flex-col w-full h-full p-2 opacity-full bg-white rounded-lg overflow-y-auto">
-		    <div onClick={()=>{
-		    	// document.querySelector("#book-modal"+props.idno).classList.add("hidden")
+		    <div class="flex justify-center hover:cursor-pointer">
+		    	<img onClick={()=>{
+		    		addToLibrary(Book)
+		    	}} style={{marginRight:95}} class="hover:h-7 hover:w-7 h-6 w-6 mr-20  rounded-full border-2 border-slate-900 p-1" src="https://cdn.iconscout.com/icon/free/png-256/library-add-1781639-1518324.png"/>
+		        <img onClick={()=>{
+		        	//close modal
+		    	// document.querySelector("#bookDetails-modal"+props.idno).classList.add("hidden")
 		    	props.updateAnimation("")
-		    	}} class="flex justify-end hover:cursor-pointer ">
-		        <img class="hover:h-6 hover:w-6 h-5 w-5 ml-20 rounded-full border-2 border-slate-900 p-1" src="https://www.freeiconspng.com/thumbs/close-icon/close-icon-47.png"/>
+		    	}} class="hover:h-7 hover:w-7 h-6 w-6  ml-10  rounded-full border-2 border-slate-900 p-1" src="https://www.freeiconspng.com/thumbs/close-icon/close-icon-47.png"/>
+		    	
 		    </div>
-		    <label class="block hover:cursor-pointer ml-auto mr-auto font-bold font-sans mt-4">Name: {book.get("title")}</label>
-		    <label class="block hover:cursor-pointer ml-auto mr-auto font-bold font-sans mt-4">Author: {book.get("author")}</label>
-		    <label class="block hover:cursor-pointer ml-auto mr-auto font-bold font-sans mt-4">Page Count: {book.get("pageCount")}</label>
-		    <label class="block hover:cursor-pointer ml-auto mr-auto font-bold font-sans mt-4">{book.get("description")}</label>
+		    <label class="block hover:cursor-pointer ml-auto mr-auto font-bold font-sans mt-4">Name: {bookDetails.get("title")}</label>
+		    <label class="block hover:cursor-pointer ml-auto mr-auto font-bold font-sans mt-4">Author: {bookDetails.get("author")}</label>
+		    <label class="block hover:cursor-pointer ml-auto mr-auto font-bold font-sans mt-4">Page Count: {bookDetails.get("pageCount")}</label>
+		    <label class="block hover:cursor-pointer ml-auto mr-auto font-bold font-sans mt-4">{bookDetails.get("description")}</label>
 		</div>
 		)
 }
